ab2fe270d426e24b3f86a7fb8768a5a8a46ce20a
Jangcccci <jangcccci@gmail.com>
2023-12-18 20:03:38 +0900

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,12 @@
 import Project from "./Project";
 
 const Projects = () => {
-  const projects = [
-    { id: 1, title: "BLABLA", content: "ffffdididiekekekdsjklsjkdfklwemfklmdklf", imgSrc: "zzzzzzz" },
-    { id: 2, title: "BLABLA", content: "ffffdididiekekekdsjklsjkdfklwemfklmdklf", imgSrc: "zzzzzzz" },
-    { id: 3, title: "BLABLA", content: "ffffdididiekekekdsjklsjkdfklwemfklmdklf", imgSrc: "zzzzzzz" },
-    { id: 4, title: "BLABLA", content: "ffffdididiekekekdsjklsjkdfklwemfklmdklf", imgSrc: "zzzzzzz" },
-  ];
-
   return (
     <section className="Projects">
       <h2 className="first-menu-name">
         Projects<span className="point-color">.</span>
       </h2>
-      <div className="project-container">
-        {projects.map((project) => (
-          <Project project={project} key={project.id} />
-        ))}
-      </div>
+      <div className="project-container"></div>
     </section>
   );
 };
